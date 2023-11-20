525d2e47041d2ab26cdce7028d91bdd04815077d
michalBassa <mb0504154191@gmail.com>
2023-11-20 15:33:03 +0200

    first commit

---
+++ Seminary.Model/Model/Student.cs
@@ -0,0 +1,28 @@
+﻿using Seminary.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminary.Model
+{
+    [Table("Student")]
+    public class Student
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string? Name { get; set; }
+        public int Age { get; set; }
+
+        public int? ClassId { get; set; }
+
+        [ForeignKey(nameof(ClassId))]
+        public ClassStudent MyClass { get; set; }
+       
+    }
+}
---
+++ Seminary.Model/Model/Teacher.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminary.Model
+{
+    [Table("Teacher")]
+    public class Teacher
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string? Name { get; set; }
+        //שנות ותק
+        public int Seniority { get; set; }
+        public int Age { get; set; }
+
+        public int? Subjectid { get; set; }
+        [ForeignKey(nameof(Subjectid))]
+        public Subject TeacherOfSubjec { get; set; }
+    }
+}
